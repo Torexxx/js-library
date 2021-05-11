679df499c4df9047ff380ad9da5596e02fe25c72
torexxx <molento-studio@yandex.ru>
2021-05-11 10:55:26 +0700

    attribute modification methods

--- src/js/lib/modules/classes.js
+++ src/js/lib/modules/classes.js
@@ -49,5 +49,24 @@ $.prototype.hasClass = function(classNames) {
     return res;
 
 
+
+}
     // TODO add/remove attributes
+
+$.prototype.addAttribute = function(name, value) {
+    for (let i = 0; i < this.length; i ++ ) { 
+        this[i].setAttribute(name, value);
+    }
+
+    return this
+
+}
+
+$.prototype.deleteAttribute = function(name, value) {
+    for (let i = 0; i < this.length; i ++ ) { 
+        this[i].removeAttribute(name);
+    }
+
+    return this
+
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import './lib/lib';
+import $ from './lib/lib';
 
  $('button').on('click', function (e) {
     $(this).toggleClass('active');
  })
 
+$('.active').addAttribute('contenteditable', '')
